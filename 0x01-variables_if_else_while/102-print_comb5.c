27adc99fdaebbeadb40b2960b611d4e795d283e3
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-11 13:00:34 -0700

    Add: task 12 files

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	n = 0;
+	while (n <= 99)
+	{
+		while (n <= 99)
+		{
+			if (m != n)
+			{
+				putchar(48 + (n / 10));
+				putchar(48 + (n % 10));
+				putchar(' ');
+				putchar(48 + (m / 10));
+				putchar(48 + (m % 10));
+				if (n != 98 || m != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			m++;
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
