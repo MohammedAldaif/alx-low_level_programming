cf240a75b05035680865d0322a49ada9834565d3
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-11 07:29:09 -0700

    Add: task 5 files

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int counter;
+
+	/* your code goes here */
+
+	counter = 0;
+	while (counter < 10)
+	{
+	printf("%d", counter);
+	counter++;
+	}
+	printf("\n");
+	return (0);
+}
