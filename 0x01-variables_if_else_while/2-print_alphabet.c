f6e51d1b6f33a35fc95c1d6e376bdc9f28b2e005
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-11 06:24:12 -0700

    Add: task 2 files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	/* your code goes there */
+	c = 'a';
+	while(c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
