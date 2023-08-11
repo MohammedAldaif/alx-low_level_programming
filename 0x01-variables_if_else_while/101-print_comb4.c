9932b35cd8b4ba138f71458d62044ed7aaae2217
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-11 12:20:35 -0700

    Add: task 11 files

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = 0 ; j <= 9 ; j++)
+		{
+			for (k = 0 ; k <= 9 ; k++)
+			{
+				if ((i < j) && (j < k))
+				{
+					putchar(48 + i);
+					putchar(48 + j);
+					putchar(48 + k);
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
