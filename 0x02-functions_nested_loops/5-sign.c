a7d16d4596214cb0be90ad5c3a932bc291c93b6a
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-15 07:25:49 -0700

    Add: task 5 files

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _isalpha - function
+ *@n: character input
+ * Return: 1 if char , 0 otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
