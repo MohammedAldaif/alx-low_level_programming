3e70ba923a45aafc683263d3f1cf92543e979d10
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-15 11:11:17 -0700

    Add: task 9 files

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * _islower - uses  _putchar to print characters
- *@c: character input
+ * add - uses  _putchar to print characters
+ *@a: input
+ *@b: input
  * Return: 1 if lower , 0 otherwise
  */
 int add(int a, int b)
 {
-	return (a+b);
-}	
+	return (a + b);
+}
