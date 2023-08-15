488bcce46372103f317bd3cbb5b56ea5679ffe8f
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-15 10:56:08 -0700

    Add: task 8 files

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 	int mul;
 	int prod;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
 		for (mul = 1; mul <= 9; mul++)
@@ -22,6 +22,7 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48);
 
 		}
 		_putchar('\n');
