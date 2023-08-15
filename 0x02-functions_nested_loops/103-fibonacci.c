d23a382c2c4d84027613d60aa23d8da71c0df0c3
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-15 16:19:07 -0700

    Add: task 15 files

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long f1;
+	unsigned long f2;
+	unsigned long sum;
+	float total;
+
+	f1 = 0;
+	f2 = 1;
+
+	while (true)
+	{
+		sum = f1 + f2;
+		if (sum > 4000000)
+			break;
+		if ((sum % 2) == 0)
+			total += sum;
+		f1 = f2;
+		f2 = sum;
+	}
+	printf("%.0f\n", total);
+	return (0);
+}
