264d4a330b3d7a2ca12ed722e7aed807e88f2bb3
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-15 15:50:36 -0700

    Add: task 13 files

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,7 @@
-#include <unistd.h>
 #include "main.h"
+
 /**
  * main - Entry point
- * @c: char
  * Return: 0
  */
 int main(void)
@@ -12,10 +11,9 @@ int main(void)
 
 	for (num = 0; num < 1024; num++)
 	{
-		if((num % 3 == 0) || (num % 5 == 0))
+		if ((num % 3 == 0) || (num % 5 == 0))
 			sum += num;
 	}
 	printf("%d\n", sum)
-	return (write(0);
+		return (0);
 }
-i
