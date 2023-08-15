0541f376b599b8c27872b6880a6a06a82f11532f
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-15 02:55:04 -0700

    Add: task 0 files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
-* main - Entry point to all programs
-*
-*
-* Alway Return(0) Success
-*/
-int main()
+ * main - Entry point to all programs
+ *
+ *
+ * Alway Return(0) Success
+ */
+int main(void)
 {
 	printf("_putchar\n");
 	return (0);
