1bd802b19f81e720ff859029c22fc4dafd14fd38
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-15 08:57:54 -0700

    Add: task 7 files

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,11 @@
  */
 int print_last_digit(int a)
 {
-	if (a < 0)
-		a *= -1;
 	a %= 10;
-	_putchar('0' + a);
-	return (a % 10);
+	if (a < 0 && a != -8)
+		a *= -1;
+	else if (a < 0 && a == -8)
+		a = 8;	
+	_putchar(48 + a);
+	return (a);
 }
--- 0x02-functions_nested_loops/test.c
+++ 0x02-functions_nested_loops/test.c
@@ -1,13 +1,13 @@
+#include <limits.h>
 #include "main.h"
 
 int main(void)
 {
+
     int r;
 
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
-    return (0);
+	r = print_last_digit(INT_MIN);
+	_putchar('0' + r);
+	_putchar('\n');
+	return (0);
 }
