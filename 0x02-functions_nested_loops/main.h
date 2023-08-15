825395e3924b5611ea7e3e822cb21eb8a87bb4a8
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-15 03:13:38 -0700

    Add: task 1 files

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,16 @@
 #ifndef MYHEADERS_H
 #define MYHEADERS_H
+#include<stdio.h>
+void print_alphabet(void);
 
+void print_alphabet(void)
+{
+	char a ='a';
+	while(a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+}
 #endif
