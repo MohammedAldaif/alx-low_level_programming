7ddeaa7ce081dd250eaa3074959b21bd734e5cdf
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-17 03:50:52 -0700

    Add: task 4 files

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		if (i == 50 || i == 52)
+			i++;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
