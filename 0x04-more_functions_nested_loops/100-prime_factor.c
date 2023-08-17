857e8b8aa10005e2458a08daa22eb827efecf8f2
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-17 12:30:52 -0700

    Add: task 11 files

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int n, fp;
+
+	n = 612852475143;
+
+	for (fp = 2; fp <= n; fp++)
+	{
+		if (n % fp == 0)
+		{
+			n /= fp;
+			fp--;
+		}
+	}
+	printf("%1d\n", fp);
+	return (0);
+}
