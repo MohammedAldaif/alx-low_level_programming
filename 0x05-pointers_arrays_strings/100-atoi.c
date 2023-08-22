d860c8065ef1740ae0f3c957d8bf209b611308ef
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-21 17:27:33 -0700

    Add: task 10 files

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+void print_rev(char *);
+
+/**
+ * _atoi - check the code for Holberton School students.
+ *@s: input
+ * Return: Always 0.
+ */
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - 0);
+		else if (num > 0)
+			break;
+		} while (*s++);
+	return (num * sign);
+}
