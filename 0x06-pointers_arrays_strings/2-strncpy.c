e7b93a833de6cae5bc830ec0b4faf339c1ca2c48
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-23 10:37:27 -0700

    Add: task 2 files

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
- * strlength - check the code for Holberton School students.
- * @str: input
- * Return: int.
- */
-
-int strlength(char *str)
-{
-	int i = 0;
-
-	while (*(str + i) != '\0')
-		i++;
-	return (i);
-}
-
-/**
  * _strncpy - check the code for Holberton School students.
  * @dest: input
  * @src: input
@@ -28,10 +13,8 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && *(src + i) != '\0'; i++)
-	{
 		*(dest + i) = *(src + i);
-	}
-	if (src[i] == '\0')
-		dest[i] = src[i];
+	for ( ; i < n; i++)
+		*(dest + i) = '\0';
 	return (dest);
 }
