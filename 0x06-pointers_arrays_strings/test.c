dd0a3d2e28ebe6981758c946455b5ea1004c23e1
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-23 03:36:54 -0700

    Add: task 0 files

---
+++ 0x06-pointers_arrays_strings/test.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    _strcat(s1,s2);
+    return (0);
+}
