37c39f19f7eb3eeda0d57df04db0e627629562e2
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-24 05:51:31 -0700

    Add: task 9 files

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,4 +15,5 @@ char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 char *leet(char *c);
+int _putchar(char c);
 #endif
