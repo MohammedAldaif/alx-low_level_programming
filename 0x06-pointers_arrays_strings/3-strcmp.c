61191d42ecdbd58166e86710f7d6bcf04f1394f1
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-24 01:31:00 -0700

    Add: task 3 files

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,8 +13,8 @@ int _strcmp(char *s1, char *s2)
 
 	while (*(s2 + i) != '\0' && *(s1 + i) != '\0')
 	{
-		if (*(s2 + i) - *(s1 + i) != 0)
-			return (*(s2 + i) - *(s1 + i));
+		if (*(s1 + i) - *(s2 + i) != 0)
+			return (*(s1 + i) - *(s2 + i));
 		i++;
 	}
 	return (0);
