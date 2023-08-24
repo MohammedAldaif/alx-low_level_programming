4abbd9f44b8cc3d255537710e8fbdd13fdf010ee
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-24 05:42:13 -0700

    Add: task 9 files

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - function
+ * @n: input
+ */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+	if (i / 10)
+		print_number(i / 10);
+	_putchar(48 + (i % 10));
+}
