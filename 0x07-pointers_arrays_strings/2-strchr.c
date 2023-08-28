43bbeccb9ef4cd441e5f26a67a1c7da6cc9ee5b5
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-28 06:02:11 -0700

    Add: task 2 files

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strchr - function
+ * @s: input
+ * @c: input
+ * Return: pointer.
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	while (c - *s != 0)
+		s++;
+	return (s);
+}
