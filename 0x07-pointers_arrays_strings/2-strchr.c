7b7b4035aba9dc26bdc476ef211732d26620799f
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-28 07:20:21 -0700

    Add: task 2 files

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,7 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	while (*(s + i) >= '\0')
-	{
-		if (*(s + i) == c)
-			return (s + i);
-		i++;
-	}
-	return (NULL);
+	while (*s - c != 0 && *s != '\0')
+		s++;
+	return (*s == '\0' ? NULL : s);
 }
