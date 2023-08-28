26999fd23eed0be6577033f2fca86252f6aa1461
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-28 04:54:34 -0700

    Add: task 0 files

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _memset - function
+ * @s: input
+ * @b: input
+ * @n: input
+ * Return: Nothing.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n--)
+		*(s + n) = b;
+	return (s);
+}
