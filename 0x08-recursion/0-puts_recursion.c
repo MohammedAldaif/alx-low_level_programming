683e8795b17ca1a7fe99939bee3795430115872b
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-30 00:55:48 -0700

    Add: task 0 files

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,15 +8,17 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
-	if (*s == NULL)
-		return;
-	else if (*s == '\0')
+	if (*s == '\0')
 	{
-		_putchar('\n');
-		return;
-	}
-	else
+		_putchar(*s);
+		s++;
+		if(*s == '\0')
+			return;
 		_puts_recursion(s);
+	}
+}
+int main(void)
+{
+    _puts_recursion("");
+    return (0);
 }
