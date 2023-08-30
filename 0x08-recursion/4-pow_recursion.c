cb9f3f69f071b0c1d4469ac10671ee562210fb06
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-30 03:42:28 -0700

    Add: task 4 files

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - check the code
+ * @x: input
+ * @y: input
+ * Return: void.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
