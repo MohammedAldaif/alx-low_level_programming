286f4849eb683d03a2acc21472faef9d6aa5706b
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-30 02:49:08 -0700

    Add: task 2 files

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,11 @@
 
 int _strlen_recursion(char *s)
 {
-	static int i = 1;
+	int i = 1;
 
 	if (*s > '\0')
 	{
-		i++, s++;
-		_strlen_recursion(s);
+		i += _strlen_recursion(s + 1) + 1;
 	}
-	return (i - 1);
+	return (i);
 }
