78ca308505252aa7cfb404d568ae11058b2b17c2
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-08-30 02:52:42 -0700

    Add: task 2 files

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,11 +8,19 @@
 
 int _strlen_recursion(char *s)
 {
-	int i = 1;
+	int i = 0;
 
 	if (*s > '\0')
 	{
 		i += _strlen_recursion(s + 1) + 1;
 	}
 	return (i);
+}
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
 }
