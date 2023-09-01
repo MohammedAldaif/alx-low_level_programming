8e8db296ed32647bc37f0b64695a54c3bea9e9de
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-09-01 08:36:21 -0700

    Add: task 3 files

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *@argc: input
+ *@argv: input
+ *Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+
+	while (i <= argc - 1)
+	{
+        	printf("%s\n", argv[i]);
+		i++;
+	}
+        return (0);
+}
