78e7cedc571a6da34013329bd75701359feed019
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-09-07 04:50:38 -0700

    M

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,14 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - function
- * @str: input
- * Return: pointer.
- */
-
-unsigned int _strlen(char *str);
-
-/**
  * string_nconcat - function
  * @s1: input
  * @s2: input
@@ -18,53 +10,28 @@ unsigned int _strlen(char *str);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int length_of_s1 = _strlen(s1);
-	unsigned int length_of_s2 = n;
-	unsigned int length_of_concatenated_string;
-	unsigned int i = 0;
-	unsigned int j = 0;
+	unsigned int length;
+	unsigned int i;
 	char *ptr;
 
-	if (n >= _strlen(s2))
-		length_of_s2 = _strlen(s2);
+	length = n;
 
-	length_of_concatenated_string = length_of_s1 + length_of_s2;
-
-	ptr = malloc((length_of_concatenated_string + 1) * sizeof(char));
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+			for (i = 0; s1[i]; i++)
+				length++;
+	ptr = malloc((length + 1) * sizeof(char));
 
 	if (ptr == NULL)
 		return (NULL);
-	/*if (_strlen(s1) == 0 && _strlen(s2) == 0) */
-		/* exit(1); */
-	for (i = 0; i < length_of_s1; i++)
-	{
-		*(ptr + i) = *(s1 + i);
-	}
-	length_of_s2--;
-	while (j <= length_of_s2)
-	{
-		*(ptr + i) = *(s2 + j);
-		i++;
-		j++;
-	}
-	*(ptr + i) = '\0';
+	length = 0;
 
+	for (i = 0; s1[i]; i++)
+		ptr[length++] = s1[i];
+	for (i = 0; s2[i] && i < n; i++)
+		ptr[length++] = s2[i];
+	ptr[length] = '\0';
 	return (ptr);
-}
-
-/**
- * _strlen - function
- * @str: input
- * Return: pointer.
- */
-
-unsigned int _strlen(char *str)
-{
-	unsigned int i = 0;
-
-	if (str == NULL)
-		str = "";
-	while (*(str + i) != '\0')
-		i++;
-	return (i);
 }
