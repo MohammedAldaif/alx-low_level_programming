c96e643abb7e47358f497a42bb2497dd12c47e7f
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-09-07 08:00:11 -0700

    m

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -4,22 +4,26 @@
 * array_range - function
 * @min: input
 * @max: input
-* Return: pointer to the address of the memory block
+* Return: pointer.
 */
 
 int *array_range(int min, int max)
 {
-	int *ptr;;
-	int i, j = 0;
+	int *ptr;
+	int i;
+	int j = 0;
+	int length;
+
+	length = max - min + 1;
 
 	if (min > max)
 		return (NULL);
-	ptr = malloc(sizeof(*ptr) * ((max - min) + 1));
+	ptr = malloc(length * sizeof(int));
 	if (ptr != NULL)
 	{
 		for (i = min; i <= max; i++)
 		{
-			ptr[j] = i;
+			*(ptr + j) = i;
 			j++;
 		}
 		return (ptr);
