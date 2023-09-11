f4e0aa76460bb95f5f556ecd103cbbba99c01c9c
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-09-11 03:56:40 -0700

    Add: task 0 files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct - struct
+ * @name: input
+ * @age: input
+ * @owner: input
+ *
+ * Description: struct.
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
