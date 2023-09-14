3b2f276a9fcef66ad7cc54b54e1bf2de52a395ea
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-09-14 02:14:37 -0700

    Add: task 1 files

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function
+ * @separator: input
+ * @n: input
+ * Return: void.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		if (*separator != '\0' && i != n - 1)
+		{
+			printf("%d%c", va_arg(list, int), *separator);
+		}
+		else
+			printf("%d", va_arg(list, int));
+	}
+	printf("\n");
+}
