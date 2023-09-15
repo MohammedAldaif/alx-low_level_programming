05742d33aa64c19d6d40383e9368ece380e7c7a0
MohammedAldaif <139805884+MohammedAldaif@users.noreply.github.com>
2023-09-15 18:59:04 +0300

    Update mymain.h

--- mymain.h
+++ mymain.h
@@ -5,6 +5,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void print_char(char c, int count);
+void print_char(char c, int chars_count);
 int _putchar(int c);
+void print_string(char* string, int chars_count);
 #endif
