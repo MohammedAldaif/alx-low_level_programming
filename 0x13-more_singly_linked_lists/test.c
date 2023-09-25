033d83479cf01aec768745fe77bc188acd8ac481
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-09-25 02:09:25 -0700

    Add: task 1 files

--- 0x13-more_singly_linked_lists/test.c
+++ 0x13-more_singly_linked_lists/test.c
@@ -1,11 +1,5 @@
 #include "lists.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
 int main(void)
 {
     listint_t *head;
@@ -23,7 +17,7 @@ int main(void)
     new->n = 9;
     new->next = head;
     head = new;
-    n = print_listint(head);
+    n = listint_len(head);
     printf("-> %lu elements\n", n);
     free(new);
     return (0);
