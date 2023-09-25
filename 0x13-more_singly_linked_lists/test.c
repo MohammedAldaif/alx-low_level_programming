e546b9b63b857ee67d7dbc4c4ed00d8eb80f3b2b
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-09-25 08:43:01 -0700

    Add: task 9 files

--- 0x13-more_singly_linked_lists/test.c
+++ 0x13-more_singly_linked_lists/test.c
@@ -3,12 +3,11 @@
 /**
  * main - check the code
  *
- * Return: Always 0.
- */
+ * Return: Always
+*/
 int main(void)
 {
     listint_t *head;
-    int sum;
 
     head = NULL;
     add_nodeint_end(&head, 0);
@@ -19,8 +18,10 @@ int main(void)
     add_nodeint_end(&head, 98);
     add_nodeint_end(&head, 402);
     add_nodeint_end(&head, 1024);
-    sum = sum_listint(head);
-    printf("sum = %d\n", sum);
+    print_listint(head);
+    printf("-----------------\n");
+    insert_nodeint_at_index(&head, 5, 4096);
+    print_listint(head);
     free_listint2(&head);
     return (0);
 }
