01df472b79bd35eff64044e48a446f89bca34019
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-09-27 02:06:48 -0700

    Add: task 13 files

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,10 +1,9 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
- *
- * Return: number of elements in the freed list
+ * free_listint_safe - function
+ * @h: pointer
+ * Return: int.
  */
 
 size_t free_listint_safe(listint_t **h)
--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,10 +1,9 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - finds the loop in a linked list
- * @head: linked list to search for
- *
- * Return: address of the node where the loop starts, or NULL
+ * find_listint_loop - function
+ * @head: pointer
+ * Return: pointer.
  */
 
 listint_t *find_listint_loop(listint_t *head)
