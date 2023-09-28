78294a47fd28c4c402aece740b0fce20964c78d1
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-09-28 08:04:07 -0700

    m

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_rev_recursion - function
+ * @s: input
+ * Return: void.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(s);
+}
