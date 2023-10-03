2ae25a074a5188b4b3650feaf571175b1d9add8d
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-10-03 11:06:40 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -34,7 +34,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		S2 = "";
+		s2 = "";
 	concat = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
 	if (concat == NULL)
 		return (NULL);
