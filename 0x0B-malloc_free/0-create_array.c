56fc6daeed72ce7043863664108695c650932800
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-10-03 09:29:13 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,13 +9,14 @@
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
 	char *ptr = malloc(sizeof(char) * size);
 
 	if (ptr == NULL)
 		return (NULL);
 	if (size == 0)
 		return (NULL);
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		ptr[i] = c;
 
 	return (ptr);
