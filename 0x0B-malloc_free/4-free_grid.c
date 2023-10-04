692e68e33357e4b1d824abc432f0f7a7bf5452a4
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-10-04 04:35:28 -0700

    4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * free_grid - function
+ * @grid: input
+ * @height: input
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
