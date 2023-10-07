3e873d35320fa57895460b891dd2d68ec53c2463
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-10-07 11:54:38 +0000

    m

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -43,7 +43,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	if (n > len2)
 		n = len2;
-	concat = malloc(sizeof(char) * len1 + n + 1);
+	concat = malloc(sizeof(char) * (len1 + n + 1));
 	if (concat == NULL)
 		return (NULL);
 
