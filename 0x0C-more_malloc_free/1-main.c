71d740434760fddc76106b5172051e0aca11dec9
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-10-07 12:03:15 +0000

    m

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 int main()
 {
 	char *concat;
 
-	concat = string_nconcat("Best ", "School !!!", 6);
+	concat = string_nconcat(NULL, NULL , 10);
+	if (concat == NULL)
+		printf("failed");
 	printf("%s\n", concat);
 	free(concat);
 	return (0);
