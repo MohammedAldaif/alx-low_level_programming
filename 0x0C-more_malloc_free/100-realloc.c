a40ee9723093a758d615564bf63ed0d6c754cb99
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-10-08 01:24:25 -0700

    4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,37 +11,37 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 
-        char *p;
-        char *copy;
-        unsigned int i;
-        unsigned int max;
+	char *p;
+	char *copy;
+	unsigned int i;
+	unsigned int max;
 
-        i = 0;
-        max = new_size;
-        copy = (char *) ptr;
+	i = 0;
+	max = new_size;
+	copy = (char *) ptr;
 
-        if (ptr == NULL)
-        {
-                p = malloc(new_size);
-                return (p);
-        }
-        if (new_size == old_size)
-                return (ptr);
-        if (new_size == 0 && ptr != NULL)
-        {
-                free(ptr);
-                return (NULL);
-        }
-        p = malloc(new_size);
-        if (p == NULL)
-                return (NULL);
-        if (new_size > old_size)
-                max = old_size;
-        while (i < max)
-        {
-                p[i] = copy[i];
-                i++;
-        }
-        free(copy);
-        return (p);
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		return (p);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	if (new_size > old_size)
+		max = old_size;
+	while (i < max)
+	{
+		p[i] = copy[i];
+		i++;
+	}
+	free(copy);
+	return (p);
 }
