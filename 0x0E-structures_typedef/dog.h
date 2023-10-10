0ad63fd3bca6c3585294581631b7827f74661387
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-10-10 06:59:39 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct
+ * @name: member
+ * @owner: member
+ * @age: member
+ * Description: simple struct
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
