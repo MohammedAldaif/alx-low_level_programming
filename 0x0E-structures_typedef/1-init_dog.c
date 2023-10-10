398370c1fc306f38b48337e84cc8ede51f4143c7
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-10-10 07:08:08 -0700

    1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - function
+ * @d : pointer to struct dog
+ * @name: pointer to char
+ * @age: input
+ * @owner: pointer to char
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
