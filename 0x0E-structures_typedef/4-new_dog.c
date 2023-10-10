8f2a04b04d0ec04bad823739ba866be90d2a938a
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-10-10 09:48:41 -0700

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,24 +1,89 @@
 #include "dog.h"
 
 /**
+ * _strlen - function
+ * @s: input
+ *
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * *_strcpy - function
+ * @dest: input
+ * @src: input
+ * Return: pointer.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
  * new_dog - function
  * @name: input
  * @age: input
  * @owner: input
- * Return: pointer to struct
+ * Return: pointer.
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *my_dog;
+	dog_t *dog;
+	int len1, len2;
 
-	my_dog = malloc(sizeof(dog_t));
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
 
-	if (my_dog == NULL)
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
 		return (NULL);
-	(*my_dog).name = strdup(name);
-	(*my_dog).owner = strdup(owner);
-	(*my_dog).age = age;
 
-	return (my_dog);
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
 }
