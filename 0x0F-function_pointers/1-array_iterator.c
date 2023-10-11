d62017a6c42bc788f4744deb265efb9119bf094e
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-10-11 06:24:35 -0700

    2

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function
+ * @array: input
+ * @size: input
+ * @action: input
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (action)
+	{
+		for (i = 0; i < size; i++)
+		{
+
+			action(array[i]);
+		}
+	}
+}
