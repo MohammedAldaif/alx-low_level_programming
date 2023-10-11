625936474e35dde27e792b527ac9284429bd27bc
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-10-11 07:53:43 -0700

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function
+ * @array: input
+ * @size: input
+ * @cmp: input
+ * Return: int.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int result;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		result = cmp(array[i]);
+
+		if (result != 0)
+			return (i);
+	}
+	return (-1);
+}
