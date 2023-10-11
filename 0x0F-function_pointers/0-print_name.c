7388a11110ba7b40594c8ecb820187ea233a6b26
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-10-11 06:08:13 -0700

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function
+ * @name: input
+ * @f: input
+ * Return:void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
