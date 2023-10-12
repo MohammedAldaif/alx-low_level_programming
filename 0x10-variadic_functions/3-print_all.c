5d9f41e2bbcb19a49d8960d3847865affc73f54c
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-10-12 12:50:31 -0700

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,136 +1,98 @@
 #include "variadic_functions.h"
-
-char *output = NULL;  
-/* Global variable to store the output */
+#include <stdlib.h>
 
 /**
- * print_char - function
- * @args: input
- * Return: void
- */
-
-void print_char(va_list args) {
-	char buffer[32];
-	snprintf(buffer, sizeof(buffer), "%c, ", va_arg(args, int));
-	output = realloc(output, strlen(output) + strlen(buffer) + 1);
-	strcat(output, buffer);
-}
-
-/**
- * print_integer - function
- * @args: input
- * Return: void
- */
-
-void print_integer(va_list args) {
-	char buffer[32];
-	snprintf(buffer, sizeof(buffer), "%d, ", va_arg(args, int));
-	output = realloc(output, strlen(output) + strlen(buffer) + 1);
-	strcat(output, buffer);
-}
-
-/**
- * print_string - function
- * @args: input
- * Return: void
- */
-
-void print_string(va_list args) {
-	char *str;
-	char buffer[7];
-
-	str = va_arg(args, char *);
-	if (str != NULL) {
-		output = realloc(output, strlen(output) + strlen(str) + 1);
-		strcat(output, str);
-		strcat(output, ", ");
-		return ;
-	}
-	buffer[0] = '(';
-	buffer[1] = 'n';
-	buffer[2] = 'i';
-	buffer[3] = 'l';
-	buffer[4] = ')';
-	buffer[5] = ',';
-	buffer[6] = ' ';
-	output = realloc(output, strlen(output) + strlen(buffer) + 1);
-	strcat(output, buffer);
-}
-
-/**
- * print_float - function
- * @args: input
- * Return: void
- */
-
-void print_float(va_list args) {
-	char buffer[64];
-	snprintf(buffer, sizeof(buffer), "%f, ", va_arg(args, double));
-	output = realloc(output, strlen(output) + strlen(buffer) + 1);
-	strcat(output, buffer);
-}
-
-/**
- * initialize_output - function
- * Return: void
- */
-
-void initialize_output() {
-	output = malloc(1);
-	output[0] = '\0';
-}
-
-/**
- * free_output - function
- * Return: void
- */
-
-void free_output() {
-	free(output);
-	output = NULL;
-}
-
-/**
- * print_all - function
- * @format: input
- * Return: void
- */
-
-void print_all(const char *format, ...) {
+  * print_all - Prints anything
+  * @format: The conversion specifier to prints
+  *
+  * Return: Nothing
+  */
+void print_all(const char * const format, ...)
+{
 	va_list args;
-	size_t output_length;
-	void (*printers[256])(va_list);
+	f_dt form_types[] = {
+		{ "c", print_a_char },
+		{ "i", print_a_integer },
+		{ "f", print_a_float },
+		{ "s", print_a_char_ptr }
+	};
+	unsigned int i = 0;
+	unsigned int j = 0;
+	char *separator = "";
 
 	va_start(args, format);
-	initialize_output();
-	printers['c'] = print_char;
-	printers['i'] = print_integer;
-	printers['s'] = print_string;
-	printers['f'] = print_float;
 
-	while (*format) {
-		if (printers[(unsigned char)*format]) {
-			printers[(unsigned char)*format](args);
+	while (format != NULL && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == *form_types[j].identifier)
+			{
+				form_types[j].f(separator, args);
+				separator = ", ";
+			}
+			j++;
 		}
-		format++;
+		i++;
 	}
 
 	va_end(args);
-
-	/* Remove the last comma and space */
-	output_length = strlen(output);
-	output[output_length - 2] = '\0';
-	printf("%s\n", output);
-	free_output();
+	printf("\n");
 }
 
 /**
- * main- entry point
- * Return: int
- */
-
-int main() {
-	print_all("ceisf", 'B', 3, "stSchool", 3.14159);
-	return 0;
+  * print_a_char - Prints a character of char type
+  * @separator: The separator of the character
+  * @args: A list of variadic arguments
+  *
+  * Return: Nothing
+  */
+void print_a_char(char *separator, va_list args)
+{
+	printf("%s%c", separator, va_arg(args, int));
 }
 
+/**
+  * print_a_integer - Prints a character of integer type
+  * @separator: The separator of the character
+  * @args: A list of variadic arguments
+  *
+  * Return: Nothing
+  */
+void print_a_integer(char *separator, va_list args)
+{
+	printf("%s%i", separator, va_arg(args, int));
+}
+
+/**
+  * print_a_float - Prints a character of float type
+  * @separator: The separator of the character
+  * @args: A list of variadic arguments
+  *
+  * Return: Nothing
+  */
+void print_a_float(char *separator, va_list args)
+{
+	printf("%s%f", separator, va_arg(args, double));
+}
+
+/**
+  * print_a_char_ptr - Prints the content of pointer to char type
+  * @separator: The separator of the character
+  * @args: A list of variadic arguments
+  *
+  * Return: Nothing
+  */
+void print_a_char_ptr(char *separator, va_list args)
+{
+	char *arg = va_arg(args, char *);
+
+	if (arg == NULL)
+	{
+		printf("%s%s", separator, "(nil)");
+		return;
+	}
+
+	printf("%s%s", separator, arg);
+}
