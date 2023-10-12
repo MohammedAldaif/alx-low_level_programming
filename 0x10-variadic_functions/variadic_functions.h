4e522c398c672fd2d4f1b5dc05067a7dfeb1902d
MohammedAldaif <mohammedaldaif71@gmail.com>
2023-10-12 14:36:56 -0700

    0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,9 @@
-#ifndef _HEADER
-#define _HEADER
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
 #include <stdio.h>
 #include <stdarg.h>
-int _putchar(char c);
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
@@ -11,14 +12,16 @@ void print_a_char(char *separator, va_list args);
 void print_a_integer(char *separator, va_list args);
 void print_a_float(char *separator, va_list args);
 void print_a_char_ptr(char *separator, va_list args);
+
 /**
- *  * struct format_types - Struct format_types
- *   * @identifier: The conversion specifier
- *    * @f: The function pointer
- *     */
+ * struct format_types - Struct format_types
+ * @identifier: The conversion specifier
+ * @f: The function pointer
+ */
+
 typedef struct format_types
 {
-	        char *identifier;
-		        void (*f)(char *separator, va_list args);
+	char *identifier;
+	void (*f)(char *separator, va_list args);
 } f_dt;
 #endif
