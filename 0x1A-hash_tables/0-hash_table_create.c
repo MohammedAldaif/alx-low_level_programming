17cdb53ed223a20cd088f16c450425613e5f772b
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2023-12-22 14:54:15 -0800

    m

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "hash_tables.h"
 
 /**
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,8 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct hash_node_s - Node of a hash table
  * @key: The key, string
@@ -31,5 +33,7 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
 
 #endif
