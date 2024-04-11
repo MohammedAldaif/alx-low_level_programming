63f2b69d058bd1358d53a2d6ae83a3dd146ec2ae
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-04-11 02:17:44 -0700

    M

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,9 +21,9 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%d] = [%d]\n", (int)i, (int)array[i]);
 		if (array[i] == value)
 			return (i);
-		printf("Value checked array[%d] = [%d]\n", (int)i, (int)array[i]);
 	}
 	return (-1);
 }
